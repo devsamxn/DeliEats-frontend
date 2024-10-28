371ceb6ca8299dbf7f2693c61f8d7385866e6e12
Sameer <negisameer1926@gmail.com>
2024-10-29 00:43:53 +0530

    hero alignment smaller screen

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,10 @@
 import hero from "../assets/hero.jpg";
-import hero2 from "../assets/hero2.jpg";
-
 const Hero = () => {
   return (
-    <div className="flex gap-2 p-2">
-      <img
-        src={hero}
-        className="mt-10 md:mt-0 max-h-[135px] md:w-full md:max-h-screen"
-      />
-      <img src={hero2} className="md:hidden max-h-[200px] max-w-[220px]" />
-    </div>
+    <img
+      src={hero}
+      className="mt-10 md:mt-0 max-h-[250px] md:w-full md:max-h-screen"
+    />
   );
 };
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,25 +16,24 @@ const HomePage = () => {
   return (
     <div className="md:grid md:grid-cols-2">
       <div className="flex flex-col gap-10 md:gap-8 md:mt-6 text-center">
-      <div className="md:hidden">
-        <Hero />
-      </div>
+        <div className="md:hidden flex justify-center">
+          <Hero />
+        </div>
         <div className="text-xl md:text-5xl font-bold font-mono tracking-tight text-orange-600 text-center">
           Tuck into a takeway today
         </div>
-        <span className="md:text-2xl font-mono">Food is just a click away!</span>
+        <span className="md:text-2xl font-mono">
+          Food is just a click away!
+        </span>
         <div className="md:px-16">
-          <SearchBar
-            placeHolder="City or Town"
-            onSubmit={handleSearchSubmit}
-          />
+          <SearchBar placeHolder="City or Town" onSubmit={handleSearchSubmit} />
         </div>
         <div className="gap-5">
-        <img src={landingImage} />
-        <div className="flex flex-col items-center justify-center gap-4 text-center">
-          <img src={appDownloadImage} className="md:w-[300px]" />
+          <img src={landingImage} />
+          <div className="flex flex-col items-center justify-center gap-4 text-center">
+            <img src={appDownloadImage} className="md:w-[300px]" />
+          </div>
         </div>
-      </div>
       </div>
       <div className="hidden md:block">
         <Hero />
