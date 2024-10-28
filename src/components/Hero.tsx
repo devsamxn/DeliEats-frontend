d4c9490ce4651cad4c8a7641c259d370e36d7d5a
Sameer <negisameer1926@gmail.com>
2024-10-29 00:35:02 +0530

    hero alignment

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,12 +3,12 @@ import hero2 from "../assets/hero2.jpg";
 
 const Hero = () => {
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 p-2">
       <img
         src={hero}
-        className="mt-10 md:mt-0 max-h-[150px] md:w-full md:max-h-screen"
+        className="mt-10 md:mt-0 max-h-[135px] md:w-full md:max-h-screen"
       />
-      <img src={hero2} className="md:hidden max-h-[200px] max-w-[230px]" />
+      <img src={hero2} className="md:hidden max-h-[200px] max-w-[220px]" />
     </div>
   );
 };
