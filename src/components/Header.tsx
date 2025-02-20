45bbeeacb7895d2a9873a04f7472b1f121608c21
Sameer <negisameer1926@gmail.com>
2025-02-20 15:48:27 +0530

    added portfolio ref

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import MainNav from "./MainNav";
 const Header = () => {
   return (
     <div className="border-b-2 border-b-orange-500 py-3">
-      <div className="container mx-auto flex justify-between items-center">
+      <div className="container mx-auto flex justify-between items-center relative">
         <Link
           to="/"
           className="text-3xl font-bold tracking-tight text-orange-500"
@@ -15,11 +15,20 @@ const Header = () => {
             src="/assets/logo.png"
           />
         </Link>
-        <div className="md:hidden">
-          <MobileNav />
-        </div>
-        <div className="hidden md:block">
-          <MainNav />
+        <div className="flex flex-row justify-around items-center gap-5">
+          <div className="md:hidden">
+            <MobileNav />
+          </div>
+          <div className="hidden md:block flex justify-around">
+            <MainNav />
+          </div>
+          <a
+            href="https://www.sameernegi.com"
+            className="font-sans italic px-2 py-1 bg-slate-300 hover:bg-slate-500 transition-colors duration-500 rounded-full"
+            target="_blank"
+          >
+            Visit my portfolio
+          </a>
         </div>
       </div>
     </div>
